84248929757e409cfa260fac655997f98b61b34d
Sameerpadhy50 <sameerpadhy50@gmail.com>
2024-02-22 17:34:06 +0530

    navbar and title bar

--- metapercept/src/App.jsx
+++ metapercept/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Navbar } from './Components/Navbar'
+import { TitleBox } from './Components/TitleBox'
 
 function App() {
  
@@ -10,6 +11,7 @@ function App() {
   return (
    <div>
     <Navbar/>
+    <TitleBox/>
    </div>
   )
 }
---
+++ metapercept/src/Components/TitleBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from "../Styles/TitleBox.module.css"
+
+export const TitleBox = () => {
+  return (
+    <div >
+        <div id={style.titlebox}>
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A aliquam ducimus ab dicta repellat nisi dignissi</p>
+        </div>
+    </div>
+  )
+}
